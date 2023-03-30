a820e28a7a48b6ebfa4d66f6db988b5f1035edd0
qianlongo <1162633511@qq.com>
2023-03-30 23:46:06 +0800

    feat: 解决分支切换的问题

--- articles/1-1-reactive/2-解决分支切换问题.js
+++ articles/1-1-reactive/2-解决分支切换问题.js
@@ -4,13 +4,15 @@ let effectStack = []
 
 function effect (fn) {
   const effectFn = () => {
+    cleanup(effectFn)
     activeEffect = effectFn
     effectStack.push(activeEffect)
     fn()
     effectStack.pop()
     activeEffect = effectStack[ effectStack.length - 1 ]
   }
-
+  // 用来装一个个的Set，当这个effectFn被执行的时候，将其从中删除
+  effectFn.deps = []
   effectFn()
 }
 
@@ -42,30 +44,47 @@ function track (target, key) {
   }
 
   activeEffect && deps.add(activeEffect)
+  activeEffect.deps.push(deps)
 }
 
 function trigger (target, key) {
   const depsMap = bucket.get(target)
 
   if (depsMap) {
-    const deps = depsMap.get(key)
+    const effects = depsMap.get(key)
+    const effectsToRun = new Set(effects)
 
-    deps.forEach((fn) => {
+    effectsToRun && effectsToRun.forEach((fn) => {
       fn && fn()
     })
   }
 }
 
+function cleanup (effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[ i ]
+
+    deps.delete(effectFn)
+  }
+
+  effectFn.deps.length = 0
+}
+
 let state = reactive({
-  name: 'fatfish',
-  age: 100
+  ok: true,
+  text: 'hello'
 })
 
 effect(() => {
-  console.log('---', state.name, state.age)
+  const text = state.ok ? state.text : 'no'
+  console.log('---', text)
 })
 
 setTimeout(() => {
-  state.name = 'vue3'
-  state.age = 1000
+  state.ok = false
+}, 1000)
+
+setTimeout(() => {
+  // 不希望这里再次被执行，因为无论text被改成什么，text的结果都是no
+  state.text = 'Vue3'
 }, 1000)
